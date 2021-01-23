720b6103609c023b4ae5b611942aea0a1832ce7c
Alexandre Oliveira <alexandre.si.ufu@gmail.com>
2021-01-23 18:38:37 -0300

    Integration github user API, add Loaders, layout adjustments

---
+++ src/core/types/User.ts
@@ -0,0 +1,15 @@
+export type UserResponse = {
+    id: number;
+    login: string;
+    name: string;
+    company: string;
+    blog: string;
+    location: string;
+    twitter_username: string;
+    public_repos: number;
+    followers: number;
+    following: number;
+    created_at: string;
+    avatar_url: string;
+    html_home: string;
+}
--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,10 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
+import dayjs from 'dayjs'
 import Button from '../../core/components/Button';
+import { UserResponse } from '../../core/types/User';
+import ImageLoader from './Loaders/ImageLoader';
+import InfoLoader from './Loaders/InfoLoader';
 import './styles.scss';
 
 const Search = () => {
     const [user, setUser] = useState('');
+    const [userResponse, setUserResponse] = useState<UserResponse>();
+    const [isLoading, setIsLoading] = useState(false);
+    const [showInfo, setShowInfo] = useState(false);
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setUser(event.target.value);
@@ -12,10 +19,14 @@ const Search = () => {
 
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        
-        // Request API
+
+        setShowInfo(true);
+        setIsLoading(true);
         axios(`https://api.github.com/users/${user}`)
-        .then(response => console.log(response));
+        .then(response => setUserResponse(response.data))
+        .finally(() => {
+            setIsLoading(false);
+        })
     }
 
     return (
@@ -36,60 +47,82 @@ const Search = () => {
                     <Button text="Encontrar" />
                 </form>
             </div>
+            { showInfo && 
             <div className="data-container">
                 <div className="profile-data">
                     <div className="profile-photo">
-                        <img 
-                            src="https://avatars.githubusercontent.com/u/2441741?v=4" 
-                            alt="Image example" 
-                            className="profile-photo-style" 
-                        />
+                        {isLoading ? <ImageLoader /> : (
+                            <>
+                                <img 
+                                    src={userResponse?.avatar_url} 
+                                    alt={userResponse?.name} 
+                                    className="profile-photo-style" 
+                                />
+                            </>
+                        )}
                     </div>
                     <div className="profile-info">
-                        <div className="repo-info">
-                            <div className="repo-info-style">
-                                <span className="repo-text">Repositórios públicos: 62</span>  
-                            </div>
-                            <div className="repo-info-style">
-                                <span className="repo-text">Seguidores: 127</span>  
-                            </div>
-                            <div className="repo-info-style">
-                                <span className="repo-text">Seguindo: 416</span>  
-                            </div>
-                        </div>
-                        <div className="personal-info">
-                            <h1 className="info-text">Informações</h1>
-                            <div className="info-container">
-                                <span className="info-row">
-                                    <span className="row-label">Empresa:&nbsp;</span> 
-                                    IFTM
-                                </span>
-                            </div>
-                            <div className="info-container">
-                                <span className="info-row">
-                                    <span className="row-label">Website/Blog:&nbsp;</span> 
-                                    asdf@gmail.com
-                                </span>
-                            </div>
-                            <div className="info-container">
-                                <span className="info-row">
-                                    <span className="row-label">Localidade:&nbsp;</span> 
-                                    Uberlândia
-                                </span>
-                            </div>
-                            <div className="info-container">
-                                <span className="info-row">
-                                    <span className="row-label">Membro desde:&nbsp;</span> 
-                                    asdf@gmail.com
-                                </span>
-                            </div>
-                        </div>
+                        {isLoading ? <InfoLoader /> : (
+                            <>
+                                <div className="repo-info">
+                                    <div className="repo-info-style">
+                                        <span className="repo-text">
+                                            Repositórios públicos: {userResponse?.public_repos}
+                                        </span>  
+                                    </div>
+                                    <div className="repo-info-style">
+                                        <span className="repo-text">
+                                            Seguidores: {userResponse?.followers}
+                                        </span>  
+                                    </div>
+                                    <div className="repo-info-style">
+                                        <span className="repo-text">
+                                            Seguindo: {userResponse?.following}
+                                        </span>  
+                                    </div>
+                                </div>
+                                <div className="personal-info">
+                                    <h1 className="info-text">Informações</h1>
+                                    <div className="info-container">
+                                        <span className="info-row">
+                                            <span className="row-label">Empresa:&nbsp;</span> 
+                                            {userResponse?.company}
+                                        </span>
+                                    </div>
+                                    <div className="info-container">
+                                        <span className="info-row">
+                                            <span className="row-label">Website/Blog:&nbsp;</span> 
+                                            {userResponse?.blog}
+                                        </span>
+                                    </div>
+                                    <div className="info-container">
+                                        <span className="info-row">
+                                            <span className="row-label">Localidade:&nbsp;</span> 
+                                            {userResponse?.location}
+                                        </span>
+                                    </div>
+                                    <div className="info-container">
+                                        <span className="info-row">
+                                            <span className="row-label">Membro desde:&nbsp;</span> 
+                                            {userResponse?.created_at}
+                                        </span>
+                                    </div>
+                                </div>
+                            </>
+                        )}
                     </div>
                 </div>
                 <div className="profile-git">
-                    <Button text="Ver perfil" />
+                    <a 
+                        href={`https://github.com/${userResponse?.login}`} 
+                        target="_blank" 
+                        className="text-link"
+                    >
+                        <Button text="Ver perfil" />
+                    </a>
                 </div>
             </div>
+            }
         </div>
     );
 }
